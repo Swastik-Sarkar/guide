db1208e9899d4a4f7db079e70f62566354b63aa6
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2025-05-06 21:57:26 +0530

    Update log_update.py

--- log_update.py
+++ log_update.py
@@ -20,7 +20,7 @@ repo = Repo(repo_path)
 commit = repo.head.commit
 commit_message = commit.message.strip()
 commit_date = commit.committed_datetime.strftime("%d-%b-%Y")
-updates_file_path = os.path.join(repo_path, 'updates.md')
+updates_file_path = os.path.join(repo_path, 'UPDATES.md')
 update_entry = f"| {commit_date} | {commit_message} |\n"
 with open(updates_file_path, 'a') as f:
     f.write(update_entry)
